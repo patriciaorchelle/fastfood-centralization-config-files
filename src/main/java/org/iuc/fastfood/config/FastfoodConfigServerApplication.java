4bda8d3345365028469905af84672aea07c5fc3c
patriciaorchelle <patriciawelehela@gmail.com>
2023-01-11 16:27:55 +0100

    first commit

---
+++ src/main/java/org/iuc/fastfood/config/FastfoodConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.iuc.fastfood.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodConfigServerApplication.class, args);
+	}
+
+}
